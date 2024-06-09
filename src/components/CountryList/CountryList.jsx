4ba0ad9c4c47abf4fc088a99fdb3a4fb53f44399
LazarenkoNatalia <lazarenkon.n.v@gmail.com>
2024-06-09 13:12:39 +0300

    task3

--- src/components/CountryList/CountryList.jsx
+++ src/components/CountryList/CountryList.jsx
@@ -1,12 +1,14 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { Grid, GridItem } from '..';
 
 export const CountryList = ({ countries }) => {
+  const location = useLocation()
+  // console.log(location)
   return (
     <Grid>
       {countries.map(({ id, country, flag }) => (
         <GridItem key={id}>
-          <Link to={`/country/${id}`}>
+          <Link to={`/country/${id}`} state={location}>
             <img src={flag} alt={country} />
           </Link>
         </GridItem>
