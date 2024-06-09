96d84360e58a27cc40120fca740ecd5a4a55d1dd
Anastasiiya Onyshchuk <shapoval.anastasiiya@gmail.com>
2024-06-09 12:16:19 +0300

    task-2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,16 @@
-import { Container, Heading, Section } from 'components';
+import { Container, CountryList, Heading, Loader, Section } from 'components';
+import useFetchCounries from 'hooks/useFetchCounries';
 
 const Home = () => {
+  const { countries, isLoading, error } = useFetchCounries();
   return (
     <Section>
       <Container>
-        <Heading title="Home" bottom />
+        {isLoading && <Loader/>}
+        {error && <Heading title="Something went wrong..." bottom />}
+
+        {countries.length > 0 && <CountryList countries={countries}/>}
+        
       </Container>
     </Section>
   );
